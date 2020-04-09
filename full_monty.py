55ccdc005dbce19b5691421ecb4b980241538bb6
Chris Jones <chris@rezenent.com>
2020-04-09 00:39:52 -0500

    add a bunch of error detection

--- full_monty.py
+++ full_monty.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 from dotenv import load_dotenv
 import glob
+import ipaddress
 import ipinfo
 import json
 import os
@@ -50,6 +51,21 @@ def get_ipinfo(ip_addy):
     details = handler.getDetails(ip_addy)
     return details
     
+    
+# Check format of IP Address
+def standardize_ip(candidate):
+    #IPv6 with a port - strip the port
+    if "[" in candidate:
+        candidate = candidate.split(']:')[0].replace("[","").replace("]","")
+
+    try:
+        ip = ipaddress.ip_address(candidate)
+        ip = candidate
+    except ValueError:
+        #ipv4 with a port - strip the port
+        ip = candidate.split(':')[0]
+    return ip
+
 
 # Begin Processing Inbound Logs
 for file in files:
@@ -59,7 +75,11 @@ for file in files:
         audit_data_dict = json.loads(row['AuditData'])
         creation_time = audit_data_dict['CreationTime']
         user_id = audit_data_dict['UserId']
-        client_ip = audit_data_dict['ClientIP']
+        
+        if audit_data_dict.get('ClientIP'):
+            client_ip = standardize_ip(audit_data_dict['ClientIP'])
+        else:
+            pass
 
         # Check Cache and Load if Missing
         if client_ip in cache:
@@ -67,19 +87,39 @@ for file in files:
             client_info = cache[client_ip]
             city = client_info['city']
             region = client_info['region']
-            country = client_info['country']
+            country_name = client_info['country_name']
         else:        
             #print("miss")
-            client_info = get_ipinfo(client_ip)
-            cache[client_ip] = {'city': client_info.city, 'region': client_info.region, 'country': client_info.country}
-            city = client_info.city
-            region = client_info.region
-            country = client_info.country
-
-        results.append([creation_time, user_id, client_ip, city, region, country])        
+            if client_ip == "<null>":
+                cache[client_ip] = { 'city': None, 'region': None, 'country_name': None }
+                city = None
+                region = None
+                country_name = None
+            else:    
+                client_info = get_ipinfo(client_ip)
+                try:
+                    city = client_info.city
+                except:
+                    city = None
+                    
+                try:
+                    region = client_info.region
+                except:
+                    region = None
+                    
+                try:
+                    country_name = client_info.country_name
+                except:
+                    country_name = None
+                    
+                cache[client_ip] = {'city': city, 'region': region, 'country_name': country_name}
+                results.append([creation_time, user_id, client_ip, city, region, country_name])        
 
   
 # Convert List of Lists to Dataframe 
-df = pd.DataFrame(results, columns = ['Creation Time', 'User Id', 'Client IP', 'City', 'Region', 'Country']) 
+df = pd.DataFrame(results, columns = ['Creation Time', 'User Id', 'Client IP', 'City', 'Region', 'Country Name']) 
+
+
+# Write to Outfile
 df.to_excel(outfile, index=False)
 
